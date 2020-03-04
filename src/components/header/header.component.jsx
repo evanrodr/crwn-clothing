0304fd93b68795f93e81e44714e9d06013b358a0
Evandro Rodrigues <evandro.rodriguescc@gmail.com>
2020-03-04 16:26:45 -0300

    fixing some components and styling

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -3,11 +3,11 @@ import { Link } from "react-router-dom";
 
 import { ReactComponent as Logo } from "../../assets/crown.svg";
 
-import "./header.component.scss";
+import "./header.styles.scss";
 
 const Header = () => (
   <div className="header">
-    <Link to="/" className="logo-container">
+    <Link className="logo-container" to="/">
       <Logo className="logo" />
     </Link>
     <div className="options">
